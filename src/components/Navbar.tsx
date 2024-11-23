e85506cba56d2357034a3d1cd9b4bb262405ae5a
Jaime Dordio <jaimedordio@gmail.com>
2024-11-23 17:29:04 +0100

    Fix nav links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -33,27 +33,19 @@ export default function Navbar() {
 
           {/* Desktop Menu */}
           <div className="hidden md:flex items-center gap-4">
-            <Link
-              href="https://creator.alonear.com/login"
-              target="_blank"
-              passHref={true}
-            >
+            <a href="https://creator.alonear.com/login" target="_blank">
               <button className="group relative overflow-hidden rounded-md bg-black-500 text-lg shadow-lg px-6 py-0.5 border-2 border-black-50">
                 <div className="absolute inset-0 bottom-0 h-0 bg-gray-400 transition-all duration-[250ms] ease-out group-hover:h-full"></div>
                 <span className="relative text-sm">{t("login")}</span>
               </button>
-            </Link>
+            </a>
 
-            <Link
-              href="https://creator.alonear.com/signup"
-              target="_blank"
-              passHref={true}
-            >
+            <a href="https://creator.alonear.com/signup" target="_blank">
               <button className="group relative overflow-hidden rounded-md bg-primary-500 text-lg shadow-lg px-6 py-0.5 border-2 border-primary-50">
                 <div className="absolute inset-0 bottom-0 h-0 bg-gray-400 transition-all duration-[250ms] ease-out group-hover:h-full"></div>
                 <span className="relative text-sm">{t("signup")}</span>
               </button>
-            </Link>
+            </a>
           </div>
 
           {/* Mobile Menu Button */}
